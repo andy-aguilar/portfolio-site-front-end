5bba2c797ef7fa11bef20ec80cb6b4b4304934ec
andy-aguilar <andres.aguilar@flatironschool.com>
2021-07-13 05:37:39 -0400

    Add container div for about page

--- src/components/about/largeAbout.js
+++ src/components/about/largeAbout.js
@@ -4,8 +4,10 @@ import "./about.css"
 export default function largeAbout() {
     return (
         <div id="about-container">
-            <div id="about" >
+            <div id="about-color" >
+                <div id="about">
                 about
+                </div>
             </div>
         </div>
     )
